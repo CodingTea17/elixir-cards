b2a99bf8c0d26384ca3d46968e01a1ebc1c18b2e
Dawson M <dawson.mortenson@gmail.com>
2018-01-12 19:51:21 -0800

    add methods to save/load files

--- lib/cards.ex
+++ lib/cards.ex
@@ -4,24 +4,38 @@ defmodule Cards do
   #   "Hocus Pocus!"
   # end
 
-    def create_deck do
-      # Elixir convention is to use double quotes
-      values = ["Ace", "Two", "Three", "Four", "Five"]
-      suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+  def create_deck do
+    # Elixir convention is to use double quotes
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
-      # List comprehension
-      for suit <- suits, value <- values do
-        "#{value} of #{suit}"
-      end
+    # List comprehension
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
     end
+  end
+  
+  # ? bool 
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
 
-    # Arity = 1
-    def shuffle(deck) do
-      Enum.shuffle(deck)
-    end
+  # Arity = 1
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
 
-    # ? bool 
-    def contains?(deck, card) do
-      Enum.member?(deck, card)
-    end
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    { status, binary } = File.read(filename)
+    :erlang.binary_to_term(binary)
+  end
 end
