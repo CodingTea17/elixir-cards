b9bf6fffe15590dcbc0d48bed539101c3925fc8d
Dawson M <dawson.mortenson@gmail.com>
2018-01-12 18:43:04 -0800

    remove default test and add a shuffle function that uses the Enum module

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,6 +1,16 @@
 defmodule Cards do
+  # def hello do
+  #   # Elixir can be implicit or explicit (e.g. "return" vs no "return")
+  #   "Hocus Pocus!"
+  # end
 
-  def hello do
-    "Hocus Pocus!"
-  end
+    def create_deck do
+      # Elixir convention is to use double quotes
+      ["Ace", "Two", "Three"] 
+    end
+
+    # Arity = 1
+    def shuffle(deck) do
+      Enum.shuffle(deck)
+    end
 end
